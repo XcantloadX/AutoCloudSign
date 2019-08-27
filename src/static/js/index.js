1e532ea8c87ddf5b8afd643e175f40dafd10e7de
XcantloadX <3188996979@qq.com>
2019-08-27 17:53:57 +0800

    更换控制面板为首页
    控制面板终于发挥了一些作用
    新增加了一些待支持的网站上去

---
+++ src/static/js/index.js
@@ -0,0 +1,63 @@
+const signer = [
+	{
+		name: "百度贴吧",
+		logo: "static/image/bdtieba.png",
+		state: "已签到",
+		link: "tieba.html",
+		class: "stat-signed"
+	},
+	
+	{
+		name: "百度知道",
+		logo: "static/image/bdzhidao.png",
+		state: "暂未实现自动签到",
+		link: "",
+		class: "stat-warn"
+	},
+	
+	{
+		name: "Bilibili",
+		logo: "static/image/bilibili.png",
+		state: "正在开发",
+		link: "",
+		class: "stat-warn"
+	},
+	
+	{
+		name: "网易云音乐",
+		logo: "static/image/cloudmusic.png",
+		state: "正在开发",
+		link: "",
+		class: "stat-warn"
+	}
+];
+
+const template = `
+<div class="signer">
+	<img class="logo" width="64" height="64" src="{logo}" alt="{name} LOGO" />
+	<p class="name">{name}</p>
+	<p>状态：<span class="{class}">{state}</span></p>
+	<a class="btn btn-link" href="{link}">查看更多</a>
+</div>
+`;
+
+//输出所有支持的网站
+function writeSigners(){
+	
+	//循环所有网站
+	for(var i = 0; i < signer.length; i++){
+		var code = template;
+		
+		if(!signer[i].link || signer[i].link == "")
+			signer[i].link = "javascript: void(0);";
+		
+		//修改参数
+		code = code.replace(/{name}/g, signer[i].name);
+		code = code.replace(/{logo}/g, signer[i].logo);
+		code = code.replace(/{link}/g, signer[i].link);
+		code = code.replace(/{state}/g, signer[i].state);
+		code = code.replace(/{class}/g, signer[i].class || "stat-signed");
+		
+		document.write(code); //输出
+	}
+}
